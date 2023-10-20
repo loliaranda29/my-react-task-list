274754e18e66b94d62081967acb90fb913f14847
Alicia Aranda <loliaranda29@gmail.com>
2023-10-19 21:37:31 -0300

    avance

--- my/src/App.jsx
+++ my/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Titulo from './componentes/Header'
 
 function App() {
 
   return (
     <>
-      
+      <Titulo/>
     </>
   )
 }
---
+++ my/src/componentes/Header.jsx
@@ -0,0 +1,5 @@
+const Titulo = () => {
+    return<h1><center>Lista de Tareas</center></h1>
+};
+
+export default Titulo
